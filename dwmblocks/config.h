505f260f252bdc778c0a2a921cc63630084e9b90
haaag <81921095+haaag@users.noreply.github.com>
2024-03-12 16:04:52 -0300

    feat(dwmblocks): add `scripts`

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,27 +1,18 @@
-// Modify this file to change what commands output to your statusbar, 
+// Modify this file to change what commands output to your statusbar,
 // and recompile using the make command.
 static const Block blocks[] = {
-/* Icon	        Command             Update Interval     Update Signal */
-    {"",        "bar-bluetooth",          60,                 12},
-    {"",        "bar-cpu",                1,                  11},
-    {"",        "bar-temppercore",        3,                  12},
-    {"",        "bar-memory",             3,                  13},
-    {"",        "bar-nettraf",            1,                  16},
-    {"",        "bar-battery",            5,                  15},
-    {"",        "bar-pulse",              0,                  10},
-    {"",        "bar-date",               3600,               19},
-    {"",        "bar-time",               10,                 25},
-    
-    // {"",        "bar-storage",         60,               21},
-    // {"",        "bar-mail",            10,                  14},
-    // {"",        "bar-bluetooth",        5,                  17},
-    // {"",        "bar-weather.sh",       3600,                  20},
-
-    // {"",        "bar-mpc",              1,                  15},
-    // {"",        "bar-systray",          3,                  18},
-    // {"",        "bar-systray-bash",     3,                  25},
+/* Icon	        Command                 Update Interval     Update Signal */
+    {"",        "bar-bluetooth",            5,                  12},
+    {"",        "bar-cpu",                  1,                  11},
+    {"",        "bar-temppercore",          3,                  12},
+    {"",        "bar-memory",               3,                  13},
+    {"",        "bar-nettraf",              1,                  16},
+    {"",        "bar-pulse",                0,                  10},
+    {"",        "bar-date",                 3600,               19},
+    {"",        "bar-time",                 10,                 25},
+    {"",        "bar-bat",                  10,                 15},
 };
 
-// Sets delimiter between status commands. 
+// Sets delimiter between status commands.
 // NULL character ('\0') means no delimiter.
 static char *delim = "";

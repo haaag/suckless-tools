2a49e9da3a332e7452c7dc07a9a65ec903161f03
haaag <81921095+haaag@users.noreply.github.com>
2024-06-25 11:28:00 -0300

    chore(dwmblocks): remove signals

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,19 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",        "bar-bluetooth",            5,                  12},
-    {"",        "bar-cpu",                  1,                  11},
-    {"",        "bar-temppercore",          3,                  12},
-    {"",        "bar-memory",               3,                  13},
-    {"",        "bar-nettraf",              1,                  16},
-    {"",        "bar-pulse",                0,                  10},
-    {"",        "bar-date",                 3600,               19},
-    {"",        "bar-time",                 10,                 25},
-    {"",        "bar-bat",                  10,                 15},
-    {"",        "bar-sys",                   5,                 17},
+	/*Icon*/	/*Command*/		    /*Update Interval*/	    /*Update Signal*/
+    {"",        "bar-pomodoro",             1,                  0},
+    {"",        "bar-bluetooth",            3,                  0},
+    {"",        "bar-cpu",                  3,                  0},
+    {"",        "bar-temppercore",          3,                  0},
+    {"",        "bar-memory",               3,                  0},
+    {"",        "bar-nettraf",              1,                  0},
+    {"",        "bar-date",                 3600,               0},
+    {"",        "bar-time",                 30,                 0},
+    {"",        "bar-volume",               0,                  10},
+    {"",        "bar-battery",              10,                 0},
+    {"",        "bar-sys",                  3,                  0},
 };
 
 // Sets delimiter between status commands.
 // NULL character ('\0') means no delimeter.
-static char *delim = " ";
+//          • ╍ 
+static char *delim = "  ";
 static unsigned int delimLen = 5;
